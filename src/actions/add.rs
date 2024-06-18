d6530477c0c99c5b771835c0f9dd61c75ea02c1d
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2024-06-19 04:01:55 +0900

    test: Init test for push_context (#93)

--- src/actions/add.rs
+++ src/actions/add.rs
@@ -1,9 +1,13 @@
 use super::command_status_ok;
 use crate::{CommandStatus, HrtorProcessor};
+use std::io::StdinLock;
 
 impl HrtorProcessor {
     pub(crate) fn add(&self) -> CommandStatus {
-        let new_context = push_context();
+        let reader: StdinLock = std::io::stdin().lock();
+        let writer: std::io::Stdout = std::io::stdout();
+
+        let new_context = push_context(reader, writer);
         {
             self.editing_file.lock().unwrap().context = new_context;
         }
@@ -12,12 +16,16 @@ impl HrtorProcessor {
 }
 
 /// get some context from standard input, and return String
-fn push_context() -> String {
+fn push_context<R, W>(mut reader: R, _writer: W) -> String
+where
+    R: std::io::BufRead,
+    W: std::io::Write,
+{
     let mut inputed_text: String = String::new();
     loop {
         let mut last_line: String = String::new();
 
-        std::io::stdin()
+        reader
             .read_line(&mut last_line)
             .expect("failed to read line");
 
@@ -33,4 +41,17 @@ fn push_context() -> String {
         inputed_text.push_str(&last_line);
     }
     inputed_text
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push_context() {
+        let mut writer: Vec<u8> = Vec::new();
+        let reader = std::io::Cursor::new(b"test\ntest\ntest\n.\n");
+        let result = push_context(reader, writer);
+        assert_eq!(result, "test\ntest\ntest\n");
+    }
 }

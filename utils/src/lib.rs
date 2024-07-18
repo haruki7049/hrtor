2aec66390fb44f6cbc3208bcdecbe006901e5ed6
haruki7049 <tontonkirikiri@gmail.com>
2024-07-18 10:13:10 +0900

    feat(utils): Init FileInfo::get_file_info function

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,4 +1,30 @@
+use std::error::Error;
+
 pub struct FileInfo {
     pub path: String,
     pub context: String,
+}
+
+impl FileInfo {
+    pub fn get_file_info(path: String) -> Result<FileInfo, Box<dyn Error>> {
+        Ok(FileInfo {
+            path: path.clone(),
+            context: std::fs::read_to_string(&path).unwrap_or_else(|_| {
+                eprintln!("Your file cannot find. Created a new buffer to continue this process.");
+                String::new()
+            }),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_file_info() {
+        let file_info = FileInfo::get_file_info("Cargo.toml".to_string()).unwrap();
+        assert_eq!(file_info.path, "Cargo.toml");
+        assert!(file_info.context.len() > 0);
+    }
 }

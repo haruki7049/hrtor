d95f40508b46c39eb97e6d4c687cee9f0abbfae1
haruki7049 <tontonkirikiri@gmail.com>
2024-07-18 11:02:32 +0900

    style: nix fmt

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,6 +1,6 @@
-use std::sync::{Arc, Mutex};
 use hrtor_utils::FileInfo;
+use std::sync::{Arc, Mutex};
 
 pub struct Hrtor {
-    pub editing_file: Arc<Mutex<FileInfo>>
+    pub editing_file: Arc<Mutex<FileInfo>>,
 }

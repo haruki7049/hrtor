921e1f089d4752f57de0ce69e0dc4b878677d1ae
haruki7049 <tontonkirikiri@gmail.com>
2024-09-28 18:47:41 +0900

    chore(flakes): Change to flake-parts

--- treefmt.nix
+++
@@ -1,7 +0,0 @@
-{
-  projectRootFile = "flake.nix";
-  programs.nixpkgs-fmt.enable = true;
-  programs.rustfmt.enable = true;
-  programs.taplo.enable = true;
-  programs.actionlint.enable = true;
-}

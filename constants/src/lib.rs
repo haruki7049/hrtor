bea5c5478a6d8e235ec515113e8b1aea1fce0ab2
haruki7049 <tontonkirikiri@gmail.com>
2024-09-30 20:45:52 +0900

    feat(constants): Update

--- constants/src/lib.rs
+++ constants/src/lib.rs
@@ -1,2 +1,13 @@
 /// PROMPT message in interpreter
 pub const PROMPT: &str = "hrtor:> ";
+
+pub enum CommandStatus {
+    Continue(CommandResult),
+    Quit,
+}
+
+pub enum CommandResult {
+    Ok,
+    NotFound(String),
+    NothingToDo,
+}

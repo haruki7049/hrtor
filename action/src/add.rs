65ed848e74dacf79cda4e4bb31f1f7744011af7b
haruki7049 <tontonkirikiri@gmail.com>
2024-09-30 20:40:24 +0900

    chore(action): Add add.rs and write.rs

---
+++ action/src/add.rs
@@ -0,0 +1,57 @@
+use super::command_status_ok;
+use crate::{CommandStatus, HrtorProcessor};
+use std::io::StdinLock;
+
+impl HrtorProcessor {
+    pub(crate) fn add(&self) -> CommandStatus {
+        let reader: StdinLock = std::io::stdin().lock();
+        let _writer: std::io::Stdout = std::io::stdout();
+
+        let new_context = push_context(reader, _writer);
+        {
+            self.editing_file.lock().unwrap().context = new_context;
+        }
+        command_status_ok()
+    }
+}
+
+/// get some context from standard input, and return String
+fn push_context<R, W>(mut reader: R, _writer: W) -> String
+where
+    R: std::io::BufRead,
+    W: std::io::Write,
+{
+    let mut inputed_text: String = String::new();
+    loop {
+        let mut last_line: String = String::new();
+
+        reader
+            .read_line(&mut last_line)
+            .expect("failed to read line");
+
+        #[cfg(unix)]
+        if last_line.as_str() == ".\n" {
+            break;
+        }
+        #[cfg(windows)]
+        if last_line.as_str() == ".\r\n" {
+            break;
+        }
+
+        inputed_text.push_str(&last_line);
+    }
+    inputed_text
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push_context() {
+        let writer: Vec<u8> = Vec::new();
+        let reader = std::io::Cursor::new(b"test\ntest\ntest\n.\n");
+        let result = push_context(reader, writer);
+        assert_eq!(result, "test\ntest\ntest\n");
+    }
+}
--- action/src/lib.rs
+++ action/src/lib.rs
@@ -1,14 +1,26 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use crate::{CommandResult, CommandStatus, HrtorProcessor};
+
+pub mod add;
+pub mod write;
+
+pub(crate) fn command_status_ok() -> CommandStatus {
+    CommandStatus::Continue(CommandResult::Ok)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl HrtorProcessor {
+    pub(crate) fn quit(&self) -> CommandStatus {
+        CommandStatus::Quit
+    }
+    pub(crate) fn delete_all(&self) -> CommandStatus {
+        {
+            let mut editing_file = self.editing_file.lock().unwrap();
+            editing_file.context.clear();
+        }
+        command_status_ok()
+    }
+    pub(crate) fn print(&self) -> CommandStatus {
+        let context = { &self.editing_file.lock().unwrap().context };
+        println!("{}", context);
+        command_status_ok()
     }
 }

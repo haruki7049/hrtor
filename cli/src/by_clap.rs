4a37a3d462572484b2d73d67ed73c8ebb7a3a676
haruki7049 <64677724+haruki7049@users.noreply.github.com>
2024-10-01 19:30:19 +0900

    test(cli): Add how_to_use_eval test (#123)

--- cli/src/by_clap.rs
+++ cli/src/by_clap.rs
@@ -16,3 +16,28 @@ impl CLI for AppArg {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::by_clap::AppArg;
+    use crate::{CLIArgs, CLI};
+    use file_loader::{CommandLineArgsParser, FileInfo};
+
+    #[test]
+    fn how_to_use_eval() {
+        let clap_cli: AppArg = AppArg {
+            path: String::from("test.txt"),
+        };
+
+        let args: CLIArgs = clap_cli.eval().unwrap();
+
+        assert_eq!(
+            FileInfo {
+                context: String::new(),
+                path: String::from("test.txt")
+            },
+            args.read_fileinfo().unwrap()
+        );
+        assert_eq!(String::from("test.txt"), args.text_file);
+    }
+}

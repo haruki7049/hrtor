eac920ce183ce4de0dca65198b118e89503d8a0b
haruki7049 <tontonkirikiri@gmail.com>
2025-05-16 22:08:22 +0900

    feat(print-action): Add line number

--- src/processor/actions/print.rs
+++ src/processor/actions/print.rs
@@ -3,7 +3,18 @@ use crate::processor::constants::CommandStatus;
 
 impl HrtorProcessor {
     pub fn print(&self, _arguments: &str) -> CommandStatus {
-        println!("{}", &self.editing_file.context);
+        let mut result: String = String::new();
+        let setp_str: Vec<&str> = self.editing_file.context.split('\n').collect(); // Separated String at '\n'
+
+        // Line Counter
+        let mut counter: usize = 0;
+        for string in setp_str {
+            counter += 1;
+            let s: String = format!("{}: {}\n", counter, string);
+            result.push_str(&s);
+        }
+
+        println!("{}", result);
 
         CommandStatus::Continue
     }
